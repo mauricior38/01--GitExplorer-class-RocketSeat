f069ac97bdcf175ef72a7468853515881870731f
Mauricio Corrêa <r38.mauricio@gmail.com>
2021-03-17 23:57:23 -0300

    first commit 01-github

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { RepositoryList } from './components/Repositorylist';
+
+import './styles/global.scss';
+
+export function App() {
+
+    return (
+        <>
+            <RepositoryList />
+        </>
+    )
+}
